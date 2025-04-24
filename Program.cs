c1b51f138780be7bc79158d3cc09c3c947c03d63
Soomin Song <song.soomin.418@outlook.com>
2025-04-24 08:53:34 -0700

    Inner loop no longer checks elements twice

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ int[] Indices(int[] nums, int target)
 {
     for (int i = 0; i < nums.Length; i++)
     {
-        for (int j = i; j < nums.Length; j++)
+        for (int j = i + 1; j < nums.Length; j++)
         {
             if (nums[i] + nums[j] == target) { return [i, j]; }
         }
