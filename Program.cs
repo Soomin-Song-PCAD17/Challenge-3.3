07eba8473171b439f41fb7338a613822cdc65fc3
Soomin Song <song.soomin.418@outlook.com>
2025-04-24 08:44:25 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/// 3. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+/// Input: nums = [2,7,11,15], target = 9
+/// Output: [0,1]
+/// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].3. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+/// Input: nums = [2,7,11,15], target = 9
+/// Output: [0,1]
+/// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+int[] nums = [2, 7, 11, 15];
+int target = 9;
+
+int[] answer = Indices(nums, target);
+Console.WriteLine($"({answer[0]}, {answer[1]})");
+
+
+int[] Indices(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target) { return [i, j]; }
+        }
+    }
+    return [];
+}
+
+
